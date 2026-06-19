f30d93b10a26a3caffcf817b6941f52f8fd2190d
joseplocal <joseplocal>
2026-06-19 01:55:35 -0500

    colores 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       colors:{
         obstetric:{
+          critical:"#BA1A1A",
+          warning:"#F59E0B",
+          success:"#16A34A",
           base:"#612853",
           secondary:"#F9CFD9"
         }
