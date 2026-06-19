c7705d640e66aeb4efac262713a26ca033b1aa87
joseplocal <joseplocal>
2026-06-18 23:21:34 -0500

    colores de figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        obstetric:{
+          base:"#612853",
+          secondary:"#F9CFD9"
+        }
+      },
+      fontFamily:{
+        sans:['Inter','Plus Jakarta Sans']
+      }
+    },
   },
   plugins: [],
 }
